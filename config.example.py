a78b75a11136599e2ff0973a9d6ebbbcb123f7ce
Lucas Bolela <lucasbolelasilveira@gmail.com>
2023-07-17 19:54:41 -0300

    Base project

---
+++ config.example.py
@@ -0,0 +1,15 @@
+config = {
+    "google_api_key": "...",
+    "youtube_playlist_id": "PL5aY_NrL1rjt_AZxj11kQjiTNLGg4ZaZA",
+    "kafka": {
+        "bootstrap.servers": "...",
+        "security.protocol": "sasl_ssl",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "...",
+        "sasl.password": "...",
+    },
+    "schema_registry": {
+        "url": "...",
+        "basic.auth.user.info": "user...:password...",
+    },
+}
